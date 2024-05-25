0c374db4901a682252839410d197e85ed640caf3
mahtabnejade <mahtabnejade@gmail.com>
2024-05-25 19:07:38 +0330

    add mpcd noslip related functions to modify!

--- noslip_Active_MPCD_streaming.cuh
+++ noslip_Active_MPCD_streaming.cuh
@@ -7,11 +7,65 @@
 
 
 
+//a function to calculate dt1 dt2 and dt3 which are dts calculated with the help of particle's velocities and distances from corresponding walls 
+__global__ void Active_mpcd_deltaT(double *vx, double *vy, double *vz, double *wall_sign_x, double *wall_sign_y, double *wall_sign_z, double *x_wall_dist, double *y_wall_dist, double *z_wall_dist, double *dt_x, double *dt_y, double *dt_z, int N){
+
+    int tid = blockIdx.x * blockDim.x + threadIdx.x;
+    if (tid<N){
+        if(wall_sign_x[tid] == 0 ) dt_x[tid] == 10000;//a big number because next step is to consider the minimum of dt .
+
+        else if(wall_sign_x[tid] == 1 || wall_sign_x[tid] == -1)  dt_x[tid] = abs(x_wall_dist[tid]/vx[tid]);
+
+        if(wall_sign_y[tid] == 0 ) dt_y[tid] == 10000;
+        else if(wall_sign_y[tid] == 1 || wall_sign_y[tid] == -1)  dt_y[tid] = abs(y_wall_dist[tid]/vy[tid]);
+
+        if(wall_sign_z[tid] == 0 ) dt_z[tid] == 10000;
+        else if(wall_sign_z[tid] == 1 || wall_sign_z[tid] == -1)  dt_z[tid] = abs(z_wall_dist[tid]/vz[tid]);
+    }
+
+
+}
+//a function to calculate minimum of 3 items  (dt_x, dt_y and dt_z) :
+__global__ void deltaT_min(double *dt_x, double *dt_y, double *dt_z, double *dt_min, int N){
+
+    int tid = blockIdx.x * blockDim.x + threadIdx.x;
+    if (tid<N){
+
+        dt_min[tid] = min(min(dt_x[tid], dt_y[tid]) , dt_z[tid]);
+        //printf("dt_min[%i] = %f", tid, dt_min[tid]);
+
+    }
+
+}
+//calculate the crossing location where the particles intersect with one wall:
+__global__ void Active_mpcd_crossing_location(double *x, double *y, double *z, double *vx, double *vy, double *vz, double *x_o, double *y_o, double *z_o, double *dt_min, double dt, double *L, int N){
+
+    int tid = blockIdx.x * blockDim.x + threadIdx.x;
+    if (tid<N){
+        if( ((x[tid] + dt * vx[tid]) >L[0]/2 || (x[tid] + dt * vx[tid])<-L[0]/2 || (y[tid] + dt * vy[tid])>L[1]/2 || (y[tid] + dt * vy[tid])<-L[1]/2 || (z[tid]+dt * vz[tid])>L[2]/2 || (z[tid] + dt * vz[tid])<-L[2]/2) && dt_min[tid]>0.1) printf("dt_min[%i] = %f\n", tid, dt_min[tid]);
+        x_o[tid] = x[tid] + vx[tid]*dt_min[tid];
+        y_o[tid] = y[tid] + vy[tid]*dt_min[tid];
+        z_o[tid] = z[tid] + vz[tid]*dt_min[tid];
+    }
+
+}
 
 
 
+__global__ void Active_mpcd_crossing_velocity(double *vx, double *vy, double *vz, double *vx_o, double *vy_o, double *vz_o, int N){
 
+    int tid = blockIdx.x * blockDim.x + threadIdx.x;
+    if (tid<N){
 
+        //calculate v(t+dt1) : in this case that we don't have acceleration it is equal to v(t).
+        //then we put the velocity equal to v(t+dt1):
+        //this part in this case is not necessary but we do it for generalization.
+        vx_o[tid] = vx[tid];
+        vy_o[tid] = vy[tid];
+        vz_o[tid] = vz[tid];
+    }
+    
+}
 
 
 
@@ -86,7 +140,7 @@ double *x_o, double *y_o ,double *z_o, double *vx_o, double *vy_o, double *vz_o,
     gpuErrchk( cudaPeekAtLastError() );
     gpuErrchk( cudaDeviceSynchronize() );
 
-    mpcd_deltaT<<<grid_size,blockSize>>>(d_vx, d_vy, d_vz, wall_sign_x, wall_sign_y, wall_sign_z, x_wall_dist, y_wall_dist, z_wall_dist, dt_x, dt_y, dt_z, N);
+    Active_mpcd_deltaT<<<grid_size,blockSize>>>(d_vx, d_vy, d_vz, wall_sign_x, wall_sign_y, wall_sign_z, x_wall_dist, y_wall_dist, z_wall_dist, dt_x, dt_y, dt_z, N, *fa_x, *fa_y, *fa_z, Nmd, mass, mass_fluid);
     gpuErrchk( cudaPeekAtLastError() );
     gpuErrchk( cudaDeviceSynchronize() );
 
@@ -94,11 +148,11 @@ double *x_o, double *y_o ,double *z_o, double *vx_o, double *vy_o, double *vz_o,
     gpuErrchk( cudaPeekAtLastError() );
     gpuErrchk( cudaDeviceSynchronize() );
 
-    mpcd_crossing_location<<<grid_size,blockSize>>>(d_x , d_y , d_z , d_vx , d_vy , d_vz, x_o, y_o, z_o, dt_min, h_mpcd, L, N);
+    Active_mpcd_crossing_location<<<grid_size,blockSize>>>(d_x , d_y , d_z , d_vx , d_vy , d_vz, x_o, y_o, z_o, dt_min, h_mpcd, L, N, *fa_x, *fa_y, *fa_z, Nmd, mass, mass_fluid);
     gpuErrchk( cudaPeekAtLastError() );
     gpuErrchk( cudaDeviceSynchronize() );
 
-    mpcd_crossing_velocity<<<grid_size,blockSize>>>(d_vx ,d_vy ,d_vz , vx_o, vy_o, vz_o, N);
+    Active_mpcd_crossing_velocity<<<grid_size,blockSize>>>(d_vx ,d_vy ,d_vz , vx_o, vy_o, vz_o, N);
     gpuErrchk( cudaPeekAtLastError() );
     gpuErrchk( cudaDeviceSynchronize() );
     
