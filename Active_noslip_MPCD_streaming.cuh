73793af29c4a50d7b2313080511c1d2d4088a870
mahtabnejade <mahtabnejade@gmail.com>
2024-06-02 19:50:39 +0330

    delete shared_mem_size parameter from the entries

--- Active_noslip_MPCD_streaming.cuh
+++ Active_noslip_MPCD_streaming.cuh
@@ -209,7 +209,7 @@ double *X_tot, double *Y_tot, double *Z_tot, double *Vx_tot, double *Vy_tot, dou
 double *CMsumblock_x, double *CMsumblock_y, double *CMsumblock_z, double *CMsumblock_mdx, double *CMsumblock_mdy, double *CMsumblock_mdz,
 double *CMsumblock_Vx, double *CMsumblock_Vy, double *CMsumblock_Vz, double *CMsumblock_mdVx, double *CMsumblock_mdVy, double *CMsumblock_mdVz, double *Xcm, double *Ycm, double *Zcm, double *Vxcm, double *Vycm, double *Vzcm, double h_mpcd, int N, int grid_size, int shared_mem_size, int blockSize_, int grid_size_,
 double *fa_x, double *fa_y, double *fa_z, double *fb_x, double *fb_y, double *fb_z ,double *ex, double *ey, double *ez,double *block_sum_ex, double *block_sum_ey, double *block_sum_ez,
-double *L, int Nmd , double ux, int mass, int mass_fluid, double real_time, int m, int topology, int shared_mem_size, double *dt_x, double *dt_y, double *dt_z, double *dt_min, 
+double *L, int Nmd , double ux, int mass, int mass_fluid, double real_time, int m, int topology, double *dt_x, double *dt_y, double *dt_z, double *dt_min, 
 double *x_o, double *y_o ,double *z_o, double *vx_o, double *vy_o, double *vz_o, double *x_wall_dist, double *y_wall_dist, double *z_wall_dist, double *wall_sign_x, double *wall_sign_y, double *wall_sign_z, double *T)
 
 {
