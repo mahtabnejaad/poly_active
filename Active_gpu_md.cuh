8ee88b92e1ed3a3ca0b0986baa15f92919214198
mahtabnejade <mahtabnejade@gmail.com>
2024-06-22 12:33:56 +0330

    comment out some print statements

--- Active_gpu_md.cuh
+++ Active_gpu_md.cuh
@@ -144,7 +144,7 @@ double *Aa_kx, double *Aa_ky, double *Aa_kz,double *Ab_kx, double *Ab_ky, double
         Ab_ky[tid]=fb_ky[tid]/mass;
         Ab_kz[tid]=fb_kz[tid]/mass;
 
-        printf("Aa_kx[%i]=%f, Aa_ky[%i]=%f, Aa_kz[%i]=%f\n", tid, Aa_kx[tid], tid, Aa_ky[tid], tid, Aa_kz[tid]);
+        //printf("Aa_kx[%i]=%f, Aa_ky[%i]=%f, Aa_kz[%i]=%f\n", tid, Aa_kx[tid], tid, Aa_ky[tid], tid, Aa_kz[tid]);
         
     }
 
@@ -168,7 +168,7 @@ __global__ void totalActive_calc_acceleration(double *Ax, double *Ay, double *Az
             Ax_tot[tid]=Ax[tid] + Aa_kx[tid] + Ab_kx[tid]; 
             Ay_tot[tid]=Ay[tid] + Aa_ky[tid] + Ab_ky[tid];
             Az_tot[tid]=Az[tid] + Aa_kz[tid] + Ab_kz[tid];
-            printf("Aa_kx[%i]=%f, Aa_ky[%i]=%f, Aa_kz[%i]=%f\n", tid, Aa_kx[tid], tid, Aa_ky[tid], tid, Aa_kz[tid]);
+            //printf("Aa_kx[%i]=%f, Aa_ky[%i]=%f, Aa_kz[%i]=%f\n", tid, Aa_kx[tid], tid, Aa_ky[tid], tid, Aa_kz[tid]);
 
         }
         else{
@@ -176,7 +176,7 @@ __global__ void totalActive_calc_acceleration(double *Ax, double *Ay, double *Az
             Ax_tot[tid]=Ax[tid]+(Aa_kx[tid]+Ab_kx[tid])*random_array[tid]; 
             Ay_tot[tid]=Ay[tid]+(Aa_ky[tid]+Ab_ky[tid])*random_array[tid];
             Az_tot[tid]=Az[tid]+(Aa_kz[tid]+Ab_kz[tid])*random_array[tid];
-            printf("Aa_kx[%i]=%f, Aa_ky[%i]=%f, Aa_kz[%i]=%f\n", tid, Aa_kx[tid], tid, Aa_ky[tid], tid, Aa_kz[tid]);
+            //printf("Aa_kx[%i]=%f, Aa_ky[%i]=%f, Aa_kz[%i]=%f\n", tid, Aa_kx[tid], tid, Aa_ky[tid], tid, Aa_kz[tid]);
         }
     }
    
