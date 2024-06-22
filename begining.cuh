8304f1c0107f09c5312cce3cdabf0737ec4b47fa
mahtabnejade <mahtabnejade@gmail.com>
2024-06-22 16:39:11 +0330

    add print_kernel into beginning

--- begining.cuh
+++ begining.cuh
@@ -275,7 +275,9 @@ curandGenerator_t gen, int grid_size, double real_time, double *gama_T, int *d_r
     
     //initMD:
     initMD(d_mdX, d_mdY , d_mdZ , d_mdVx , d_mdVy , d_mdVz , d_mdAx , d_mdAy , d_mdAz , _holder , d_Fy_holder , d_Fz_holder , d_L , ux ,pos , n_md , m_md , topology , density);
-   
+    
+    print_kernel<<<grid_size,blockSize>>>(d_mdX, d_mdY, d_mdZ, Nmd);
+    printf("***the initial values***\n");
    
     Active_calc_acceleration(d_mdX , d_mdY, d_mdZ , _holder , d_Fy_holder , d_Fz_holder , d_mdAx , d_mdAy , d_mdAz ,d_fa_kx, d_fa_ky, d_fa_kz, d_fb_kx, d_fb_ky, d_fb_kz, d_Aa_kx, d_Aa_ky, d_Aa_kz, d_Ab_kx, d_Ab_ky, d_Ab_kz, d_ex, d_ey, d_ez, ux, density, gama_T, d_L , Nmd , m_md ,topology , real_time, grid_size,1 ,
      N, d_random_array, seed, d_Ax_tot, d_Ay_tot, d_Az_tot, h_fa_x, h_fa_y, h_fa_z, h_fb_x, h_fb_y, h_fb_z, d_block_sum_ex, d_block_sum_ey, d_block_sum_ez, flag_array, u_scale);
