6b14bc9a12159ddeac055ae5d0936d544a56cc91
mahtabnejade <mahtabnejade@gmail.com>
2024-06-29 16:11:20 +0330

    change the order of function calls in Active_noslip_md_velocityverletKernel3 and make v=-v_o in lab frame not in CM frame

--- Active_noslip_MD_streaming.cuh
+++ Active_noslip_MD_streaming.cuh
@@ -773,6 +773,7 @@ __global__ void Active_CM_md_bounceback_velocityverlet1(double *mdx, double *mdy
 
 }
 
+
 //Active_CM_particle_on_box_and_reverse_velocity_and_md_bounceback_velocityverlet1
 __global__ void Active_CM_particle_on_box_and_reverse_velocity_and_md_bounceback_velocityverlet1(double *mdx, double *mdy, double *mdz, double *mdx_o, double *mdy_o, double *mdz_o, double *mdvx, double *mdvy, double *mdvz, double *mdvx_o, double *mdvy_o, double *mdvz_o, double *mdAx_tot, double *mdAy_tot, double *mdAz_tot, double *md_dt_min, double md_dt, double *L, int Nmd, double *Xcm, double *Ycm, double *Zcm, int *errorFlag){
 
