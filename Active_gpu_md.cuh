0170a25c9150546a9b55c1f682312f6398d7db36
mahtabnejade <mahtabnejade@gmail.com>
2024-08-02 05:27:03 +0330

    add Fx_bend, ... where necessary

--- Active_gpu_md.cuh
+++ Active_gpu_md.cuh
@@ -701,6 +701,7 @@ __host__ void Active_MD_streaming(double *d_mdX, double *d_mdY, double *d_mdZ,
     double *CMsumblock_x, double *CMsumblock_y, double *CMsumblock_z, double *CMsumblock_mdx, double *CMsumblock_mdy, double *CMsumblock_mdz,
     double *CMsumblock_Vx, double *CMsumblock_Vy, double *CMsumblock_Vz, double *CMsumblock_mdVx, double *CMsumblock_mdVy, double *CMsumblock_mdVz,
     double *d_Fx, double *d_Fy, double *d_Fz,
+    double *d_Fx_bend, double *d_Fy_bend, double *d_Fz_bend,
     double *d_fa_kx, double *d_fa_ky, double *d_fa_kz,
     double *d_fb_kx, double *d_fb_ky, double *d_fb_kz,
     double *d_Aa_kx, double *d_Aa_ky, double *d_Aa_kz,
@@ -746,7 +747,7 @@ __host__ void Active_MD_streaming(double *d_mdX, double *d_mdY, double *d_mdZ,
         //These accelerations are used in the subsequent time step to update particle velocities.
         //***
         Active_calc_acceleration( d_mdX ,d_mdY , d_mdZ , 
-        d_Fx , d_Fy , d_Fz,
+        d_Fx , d_Fy , d_Fz, d_Fx_bend , d_Fy_bend , d_Fz_bend,
         d_mdAx , d_mdAy, d_mdAz, d_fa_kx, d_fa_ky, d_fa_kz, d_fb_kx, d_fb_ky, d_fb_kz,
         d_Aa_kx, d_Aa_ky, d_Aa_kz, d_Ab_kx, d_Ab_ky, d_Ab_kz, d_ex, d_ey, d_ez,
         ux, mass, gama_T, d_L, Nmd , m , topology, real_time,  grid_size, mass_fluid, N, random_array, seed, d_Ax_tot, d_Ay_tot, d_Az_tot, h_fa_x, h_fa_y, h_fa_z, h_fb_x, h_fb_y, h_fb_z, d_block_sum_ex, d_block_sum_ey, d_block_sum_ez, flag_array, u_scale, K_FENE, K_bend);
