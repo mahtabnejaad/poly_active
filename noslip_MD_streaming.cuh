c8e5785690d4a7db61ffe3ff3a15b6e02bb42491
mahtabnejade <mahtabnejade@gmail.com>
2024-08-03 14:59:21 +0330

    change and modify bending_interaction

--- noslip_MD_streaming.cuh
+++ noslip_MD_streaming.cuh
@@ -132,50 +132,102 @@ double *L,int size , double ux, double mass, double real_time, int m , int topol
 {
 
     int tid = blockIdx.x * blockDim.x + threadIdx.x ;
+    
     if (tid<size){
 
         int loop = int(tid/m);
         int ID = tid%m;
-        double Ri1[3];
+        double Ri_2[3];
+        double Ri_1[3];
         double Ri[3];
+        double Ri1[3];
+        double ri[3];
+        double r2;
+        double dot_product;
 
         if(ID == 0){
-
-            regular_distance(mdX[tid], mdY[tid], mdZ[tid] , mdX[tid+1] , mdY[tid+1] , mdZ[tid+1] , Ri1, L, ux, real_time);
             
-            regular_distance(mdX[tid-1], mdY[tid-1], mdZ[tid-1] , mdX[tid] , mdY[tid] , mdZ[tid] , Ri, L, ux, real_time);
+            regular_distance(mdX[tid+1], mdY[tid+1], mdZ[tid+1] , mdX[tid+2] , mdY[tid+2] , mdZ[tid+2] , Ri1, L, ux, real_time);
+            
+            regular_distance(mdX[tid], mdY[tid], mdZ[tid] , mdX[tid+1] , mdY[tid+1] , mdZ[tid+1] , Ri, L, ux, real_time);
+
+            regular_distance(mdX[m*loop-1], mdY[m*loop-1], mdZ[m*loop-1] , mdX[tid] , mdY[tid] , mdZ[tid] , Ri_1, L, ux, real_time);
+
+            regular_distance(mdX[m*loop-2], mdY[m*loop-2], mdZ[m*loop-2] , mdX[m*loop-1] , mdY[m*loop-1] , mdZ[m*loop-1] , Ri_2, L, ux, real_time);
 
 
-             
+            
+
+        if(ID == 1){
+
+            regular_distance(mdX[tid+1], mdY[tid+1], mdZ[tid+1] , mdX[tid+2] , mdY[tid+2] , mdZ[tid+2] , Ri1, L, ux, real_time);
+            
+            regular_distance(mdX[tid], mdY[tid], mdZ[tid] , mdX[tid+1] , mdY[tid+1] , mdZ[tid+1] , Ri, L, ux, real_time);
+
+            regular_distance(mdX[tid-1], mdY[tid-1], mdZ[tid-1] , mdX[tid] , mdY[tid] , mdZ[tid] , Ri_1, L, ux, real_time);
+
+            regular_distance(mdX[m*loop-1], mdY[m*loop-1], mdZ[m*loop-1] , mdX[m*loop] , mdY[m*loop] , mdZ[m*loop] , Ri_2, L, ux, real_time);
+
+        }
         }
         else if(ID == (m-1)){
 
-            regular_distance(mdX[tid], mdY[tid], mdZ[tid] , mdX[m*loop] , mdY[m*loop] , mdZ[m*loop] , Ri1, L, ux, real_time);
+            regular_distance(mdX[m*loop], mdY[m*loop], mdZ[m*loop] , mdX[m*loop+1] , mdY[m*loop+1] , mdZ[m*loop+1] , Ri1, L, ux, real_time);
             
-            regular_distance(mdX[(loop+1)*m-1], mdY[(loop+1)*m-1], mdZ[(loop+1)*m-1] , mdX[tid] , mdY[tid] , mdZ[tid] , Ri, L, ux, real_time);
+            regular_distance(mdX[tid], mdY[tid], mdZ[tid] , mdX[m*loop] , mdY[m*loop] , mdZ[m*loop] , Ri, L, ux, real_time);
 
+            regular_distance(mdX[tid-1], mdY[tid-1], mdZ[tid-1] , mdX[tid] , mdY[tid] , mdZ[tid] , Ri_1, L, ux, real_time);
 
-        }
-        else if(ID < m-1){
-
-            regular_distance(mdX[tid], mdY[tid], mdZ[tid] , mdX[tid+1] , mdY[tid+1] , mdZ[tid+1] , Ri1, L, ux, real_time);
-            
-            regular_distance(mdX[tid-1], mdY[tid-1], mdZ[tid-1] , mdX[tid] , mdY[tid] , mdZ[tid] , Ri, L, ux, real_time);
-
+            regular_distance(mdX[tid-2], mdY[tid-2], mdZ[tid-2] , mdX[tid-1] , mdY[tid-1] , mdZ[tid-1] , Ri_2, L, ux, real_time);
 
 
         }
 
-        fx_bend[tid] = -K_bend*(Ri1[0]-Ri[0]);
-        fy_bend[tid] = -K_bend*(Ri1[1]-Ri[1]);
-        fz_bend[tid] = -K_bend*(Ri1[2]-Ri[2]);
+        else if(ID == (m-2)){
+
+
+            regular_distance(mdX[tid+1], mdY[tid+1], mdZ[tid+1] , mdX[m*loop] , mdY[m*loop] , mdZ[m*loop] , Ri1, L, ux, real_time);
+            
+            regular_distance(mdX[tid], mdY[tid], mdZ[tid] , mdX[tid+1] , mdY[tid+1] , mdZ[tid+1] , Ri, L, ux, real_time);
+
+            regular_distance(mdX[tid-1], mdY[tid-1], mdZ[tid-1] , mdX[tid] , mdY[tid] , mdZ[tid] , Ri_1, L, ux, real_time);
+
+            regular_distance(mdX[tid-2], mdY[tid-2], mdZ[tid-2] , mdX[tid-1] , mdY[tid-1] , mdZ[tid-1] , Ri_2, L, ux, real_time);
+
+
+
+        }
+        else if(1 < ID < m-2){
+
+            regular_distance(mdX[tid+1], mdY[tid+1], mdZ[tid+1] , mdX[tid+2] , mdY[tid+2] , mdZ[tid+2] , Ri1, L, ux, real_time);
+            
+            regular_distance(mdX[tid], mdY[tid], mdZ[tid] , mdX[tid+1] , mdY[tid+1] , mdZ[tid+1] , Ri, L, ux, real_time);
+
+            regular_distance(mdX[tid-1], mdY[tid-1], mdZ[tid-1] , mdX[tid] , mdY[tid] , mdZ[tid] , Ri_1, L, ux, real_time);
+
+            regular_distance(mdX[tid-2], mdY[tid-2], mdZ[tid-2] , mdX[tid-1] , mdY[tid-1] , mdZ[tid-1] , Ri_2, L, ux, real_time);
+
+
+
+        }
+
+        ri[0]=mdX[tid];
+        ri[1]=mdY[tid];
+        ri[2]=mdZ[tid];
+
+        r2 = ri[0]*ri[0]+ri[1]*ri[1]+ri[2]*ri[2];
+        dot_product = (3*Ri_1[0] - 3*Ri[0] + Ri1[0] - Ri_2[0])*ri[0] + (3*Ri_1[1] - 3*Ri[1] + Ri1[1] - Ri_2[1])*ri[1] + (3*Ri_1[2] - 3*Ri[2] + Ri1[2] - Ri_2[2])*ri[2];
+
+        fx_bend[tid] = -K_bend*ri[0]*dot_product/r2;
+        fy_bend[tid] = -K_bend*ri[1]*dot_product/r2;
+        fz_bend[tid] = -K_bend*ri[2]*dot_product/r2;
+
 
         fx[tid] = fx[tid] + fx_bend[tid];
         fy[tid] = fy[tid] + fy_bend[tid];
         fz[tid] = fz[tid] + fz_bend[tid];
 
     }
-
 
 }
 
