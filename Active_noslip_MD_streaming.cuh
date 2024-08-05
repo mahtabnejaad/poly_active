11b7a8fcc4ffd2a86862e76bb4fb2323003446b5
mahtabnejade <mahtabnejade@gmail.com>
2024-08-05 14:14:47 +0330

    include tid=0 as well

--- Active_noslip_MD_streaming.cuh
+++ Active_noslip_MD_streaming.cuh
@@ -625,11 +625,11 @@ double *L,int size , double ux, double mass, double real_time, int m , int topol
             fz_bend[tid] = -K_bend*ri[2]*dot_product/r2;
         }
 
-        if(tid !=0){
+        
             fx[tid] = fx[tid] + fx_bend[tid];
             fy[tid] = fy[tid] + fy_bend[tid];
             fz[tid] = fz[tid] + fz_bend[tid];
-        }
+        
 
     }
 
