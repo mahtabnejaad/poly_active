4018f28b3dae9649682c1f708eb2ba7d1186fccc
mahtabnejade <mahtabnejade@gmail.com>
2024-08-24 15:39:19 +0330

    define hosterrorflag_opp_opp

--- Active_noslip_MD_streaming.cuh
+++ Active_noslip_MD_streaming.cuh
@@ -2228,7 +2228,7 @@ __host__ void Active_noslip_MD_streaming2(double *d_mdX, double *d_mdY, double *
     double *d_fa_kx, double *d_fa_ky, double *d_fa_kz, double *d_fb_kx, double *d_fb_ky, double *d_fb_kz, double *d_Aa_kx, double *d_Aa_ky, double *d_Aa_kz, double *d_Ab_kx, double *d_Ab_ky, double *d_Ab_kz, double *d_Ax_tot, double *d_Ay_tot, double *d_Az_tot, double *d_Ax_tot_lab, double *d_Ay_tot_lab, double *d_Az_tot_lab, double *d_ex, double *d_ey, double *d_ez,
     double *h_fa_x, double *h_fa_y, double *h_fa_z, double *h_fb_x, double *h_fb_y, double *h_fb_z, double *Ax_cm, double *Ay_cm, double *Az_cm, double *d_block_sum_ex, double *d_block_sum_ey, double *d_block_sum_ez,
     double h_md, int Nmd, int m_md, int N, double mass, double mass_fluid, double *d_L , double ux, int grid_size, int shared_mem_size, int shared_mem_size_, int blockSize_, int grid_size_, int delta, double real_time, double *gama_T, int *random_array, unsigned int seed, int topology, int *flag_array, double u_scale,
-    double *md_dt_min, double *md_dt_x, double *md_dt_y, double *md_dt_z, double *md_dt_min_opp, double *md_dt_x_opp, double *md_dt_y_opp, double *md_dt_z_opp, double *mdX_o, double *mdY_o, double *mdZ_o, double *mdvx_o, double *mdvy_o, double *mdvz_o, double *mdX_o_opp, double *mdY_o_opp, double *mdZ_o_opp, double *mdvx_o_opp, double *mdvy_o_opp, double *mdvz_o_opp, double *mdX_wall_dist, double *mdY_wall_dist, double *mdZ_wall_dist, double *wall_sign_mdX, double *wall_sign_mdY, double *wall_sign_mdZ, int *hostErrorFlag, int *hostErrorFlag_opp, int *n_out_flag, int *n_out_flag_opp, double *d_zero, double K_FENE, double K_bend, double K_l){
+    double *md_dt_min, double *md_dt_x, double *md_dt_y, double *md_dt_z, double *md_dt_min_opp, double *md_dt_x_opp, double *md_dt_y_opp, double *md_dt_z_opp, double *mdX_o, double *mdY_o, double *mdZ_o, double *mdvx_o, double *mdvy_o, double *mdvz_o, double *mdX_o_opp, double *mdY_o_opp, double *mdZ_o_opp, double *mdvx_o_opp, double *mdvy_o_opp, double *mdvz_o_opp, double *mdX_wall_dist, double *mdY_wall_dist, double *mdZ_wall_dist, double *wall_sign_mdX, double *wall_sign_mdY, double *wall_sign_mdZ, int *hostErrorFlag, int *hostErrorFlag_opp, int *hostErrorFlag_opp_opp, int *n_out_flag, int *n_out_flag_opp, double *d_zero, double K_FENE, double K_bend, double K_l){
 
         for (int tt = 0 ; tt < delta ; tt++)
     {
@@ -2241,7 +2241,7 @@ __host__ void Active_noslip_MD_streaming2(double *d_mdX, double *d_mdY, double *
         Xcm, Ycm, Zcm, Vxcm, Vycm, Vzcm, Xcm_out, Ycm_out, Zcm_out, Vxcm_out, Vycm_out, Vzcm_out,
         h_md, Nmd, N, n_outbox_md, n_outbox_mpcd, mass, mass_fluid, d_L, grid_size, shared_mem_size, shared_mem_size_, blockSize_, grid_size_, md_dt_x, md_dt_y, md_dt_z, md_dt_min, md_dt_x_opp, md_dt_y_opp, md_dt_z_opp, md_dt_min_opp,
         mdX_o, mdY_o, mdZ_o, mdvx_o, mdvy_o, mdvz_o, mdX_o_opp, mdY_o_opp, mdZ_o_opp, mdvx_o_opp, mdvy_o_opp, mdvz_o_opp, d_Ax_tot, d_Ay_tot, d_Az_tot, d_Ax_tot_lab, d_Ay_tot_lab, d_Az_tot_lab,
-        mdX_wall_dist, mdY_wall_dist, mdZ_wall_dist, wall_sign_mdX, wall_sign_mdY, wall_sign_mdZ, hostErrorFlag, hostErrorFlag_opp, n_out_flag, n_out_flag_opp, d_zero);
+        mdX_wall_dist, mdY_wall_dist, mdZ_wall_dist, wall_sign_mdX, wall_sign_mdY, wall_sign_mdZ, hostErrorFlag, hostErrorFlag_opp, hostErrorFlag_opp_opp, n_out_flag, n_out_flag_opp, d_zero);
         
         
         //The function calc_accelaration is called to compute the new accelerations for each particle based on their positions and interactions.
